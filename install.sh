33038bb9edf5501ecb6039977ef923dec73f5cce
JordaanI <jordaanivan@gmail.com>
2026-04-09 22:04:03 -0400

    added general install

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/env zsh
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Requires <sudo>"
+    exit 1                                                                                                                              
+fi
+
+./nginx.sh
+./tailscale.sh
